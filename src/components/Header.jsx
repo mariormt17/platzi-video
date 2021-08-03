58181b7a80dd88e1ef5c37b17b164ce2d5b38749
Mario Mancilla <mariormt17@gmail.com>
2021-08-02 20:38:36 -0500

    Header created and its styles added

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../assets/styles/components/Header.scss'
+//import LogoPlatziVideo from '../assets/static/logo-platzi-video-BW2.png';
+//import ProfileIcon from '../assets/static/user-icon.png';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img className="header__img" src="" alt="platzi-video-logo" />
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          <img src="" alt="profile-icon" />
+          <p>Profile</p>
+        </div>
+        <ul>
+          <li><a href="/">Account</a></li>
+          <li><a href="/">Log out</a></li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
