4523aff530a0770a01eac88f88399cec8a4ed4cc
Mario Mancilla <mariormt17@gmail.com>
2021-08-03 13:58:28 -0500

    Add the logo with colors in the main section

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../assets/styles/components/Search.scss'
+import logoColors from '../assets/static/logo-platzi-video-colors.png';
 
 const Search = () => {
   return (
     <section className="main">
+      <img className="main__logo" src={logoColors} alt="Logo Platzi Video" />
       <h2 className="main__title">What do you want to search?</h2>
       <input type="text" className="input" placeholder="Search..." />
     </section>
